1066749b8dfc5d3fba7326f9ef4341667e31a6ac
Thilak Kumar <thilak.kumar@accionlabs.com>
2019-09-18 17:13:31 +0530

    Samle

---
+++ SampleDemoo/SampleDemoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleDemoo
+//
+//  Created by Thilak kumar on 18/09/19.
+//  Copyright © 2019 Thilak kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
